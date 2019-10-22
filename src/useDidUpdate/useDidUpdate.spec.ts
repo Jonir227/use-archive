2182c0be8bb73fd5a51a7456905ac522aa4143c2
Kim Bum June <bjbj6363@gmail.com>
2019-10-22 15:00:19 +0900

    useDidUpdate 테스트, 코드 리팩토링

--- src/useDidUpdate/useDidUpdate.spec.ts
+++ src/useDidUpdate/useDidUpdate.spec.ts
@@ -1,4 +1,37 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useDidUpdate from './useDidUpdate';
+
+const testDidUpdate = (toBe: boolean) => (
+  prev: any,
+  next: any,
+  fn?: (prev: any, next: any) => boolean,
+) => {
+  let success = false;
+  const checkFunction = () => {
+    success = true;
+  };
+  const { result, rerender } = renderHook(({ data }) => useDidUpdate(data, checkFunction, fn), {
+    initialProps: { data: prev },
+  });
+  rerender({ data: next });
+  expect(success).toBe(toBe);
+};
+
+const testUpdate = testDidUpdate(true);
+const testNotUpdate = testDidUpdate(false);
+
 describe('useDidUpdate', () => {
-  it.todo('update if deps are not equal with shallow equality');
-  it.todo('block update if deps are not equal with shallow equality');
+  it('update if deps are not equal', () => {
+    testUpdate('hi', 'notHI');
+    testUpdate({ data: 123 }, { data: 12345 });
+  });
+  it('block update if deps are equal', () => {
+    testNotUpdate('a', 'a');
+    const obj = { data: 'aaa' };
+    testNotUpdate(obj, obj);
+  });
+  it('compares with provided compare function', () => {
+    testNotUpdate('a', 'b', () => false);
+    testUpdate('a', 'b', () => true);
+  });
 });
--- src/useDidUpdate/useDidUpdate.ts
+++ src/useDidUpdate/useDidUpdate.ts
@@ -5,12 +5,17 @@ import usePrevious from '../usePrevious';
  * 이전 값과 비교하여 실행하는 함수
  * @param value 얕은 비교를 실행할 값
  * @param callback 값이 변경되었을때 실행할 함수
+ * @param compare 값을 비교하는 함수. 기본은 !== 로 비교한다.
  */
-const useDidUpdate = <T>(value: T, callback: (prevValue: T, value: T) => void) => {
+const useDidUpdate = <T>(
+  value: T,
+  callback: (prevValue: T, value: T) => void,
+  compare: (prev: T, next: T) => boolean = (p, n) => p !== n,
+) => {
   const prevValue = usePrevious(value);
 
   useEffect(() => {
-    if (prevValue && prevValue !== value) {
+    if (prevValue && compare(prevValue, value)) {
       callback(prevValue, value);
     }
   }, [value]);
